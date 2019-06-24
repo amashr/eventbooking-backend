d1982f4029ecc0fce02db9c186cea8854c8234ea
FrugenceFide <frugencefidel@gmail.com>
2019-06-24 20:25:50 +0300

    Add createUser functionality

--- src/resolvers/Mutation.js
+++ src/resolvers/Mutation.js
@@ -1,3 +1,6 @@
+const bcrypt = require('bcryptjs');
+const jwt = require('jsonwebtoken');
+
 const Mutation = {
   async createEvent(root, args, ctx) {
     const event = await ctx.prisma.createEvent({
@@ -7,6 +10,24 @@ const Mutation = {
       price: args.price
     });
     return event;
+  },
+  async signup(root, args, ctx) {
+    args.email = args.email.toLowerCase();
+    args.password = await bcrypt.hash(args.password, 10);
+
+    const user = await ctx.prisma.createUser({
+      name: args.name,
+      email: args.email,
+      password: args.password
+    });
+    // Create jwt token
+    const token = jwt.sign({ userId: user.id }, process.env.APP_SECRET);
+    // jwt token as cookie on response
+    // ctx.response.cookie('token', token, {
+    //   httpOnly: true,
+    //   maxAge: 1000 * 60 * 60 * 24 * 365 // 1 year cookie
+    // });
+    return user;
   }
 };
 
