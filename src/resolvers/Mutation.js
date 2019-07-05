cd73587ad1f1d843b3bafa678d86f34f15dd6f28
FrugenceFide <frugencefidel@gmail.com>
2019-07-05 14:13:03 +0300

    Signin user

--- src/resolvers/Mutation.js
+++ src/resolvers/Mutation.js
@@ -28,6 +28,26 @@ const Mutation = {
       maxAge: 1000 * 60 * 60 * 24 * 365
     });
     return user;
+  },
+  async signin(root, args, ctx) {
+    const { email, password } = args;
+    const user = await ctx.prisma.user({ email });
+
+    if (!user) {
+      throw new Error(`No such user found for email ${email}`);
+    }
+
+    const valid = await bcrypt.compare(password, user.password);
+    if (!valid) {
+      throw new Error('Invalid password');
+    }
+
+    const token = jwt.sign({ userId: user.id }, process.env.APP_SECRET);
+    ctx.response.cookie('token', token, {
+      httpOnly: true,
+      maxAge: 1000 * 60 * 60 * 24 * 365
+    });
+    return user;
   }
 };
 
